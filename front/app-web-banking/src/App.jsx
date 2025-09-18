5aab8c983718bef9707e345089442546673a853d
Leonardo Villagran <leonardo.nvillagrans@gmail.com>
2025-09-18 18:55:24 -0500

    BANK-20250918-1854 [feature]: Installed and powered by Tailwind

--- front/app-web-banking/src/App.jsx
+++ front/app-web-banking/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import './index.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h3>Hola mundo desde react</h3>
+      <div className="flex flex-col items-center justify-center min-h-screen p-4">
+       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+          Button
+        </button>
+      </div>
     </>
   )
 }
